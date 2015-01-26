c785234096bdd7df59f2bf202f98e1d1f6a31945
XSiang <isheephat@gmail.com>
2015-01-26 17:37:03 +0800

    first commit

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.confirm.subject
+  #
+  def confirm(email)
+        @message = "Thank you for confirmation!"
+        mail(:to => email, :subject => "Registered")
+  end
+end
